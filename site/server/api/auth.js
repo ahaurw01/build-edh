89a96e1552576438ed7cfdbb7b4414c7d45e8eaa
ahaurw01 <aaron.haurwitz@gmail.com>
2019-02-02 15:04:15 -0500

    get register/login working properly

--- site/server/api/auth.js
+++ site/server/api/auth.js
@@ -1,19 +1,43 @@
+const bcrypt = require('bcrypt')
 const _ = require('lodash')
 const jwt = require('jsonwebtoken')
+const { User } = require('./models')
+const jwtSecret = 'secret'
 
 module.exports = {
   login,
+  register,
 }
 
-function login(ctx, next) {
-  if (
-    _.get(ctx, 'request.body.username') === 'aaron' &&
-    _.get(ctx, 'request.body.password') === 'password'
-  ) {
-    ctx.body = {
-      token: jwt.sign({ userId: 123 }, 'secret'),
-    }
-  } else {
-    return next()
+async function login(ctx, next) {
+  const username = _.get(ctx, 'request.body.username')
+  const password = _.get(ctx, 'request.body.password')
+  const user = await User.findOne({ username })
+  if (!user) return next()
+  const match = await bcrypt.compare(password, user.passwordHash)
+  if (!match) return next()
+
+  ctx.body = {
+    token: jwt.sign(user.safeProps(), jwtSecret),
+  }
+}
+
+async function register(ctx, next) {
+  const username = _.get(ctx, 'request.body.username', '').trim()
+  const password = _.get(ctx, 'request.body.password')
+  if (!username || !password) {
+    return ctx.throw(400, 'Invalid data')
+  }
+
+  const existingUsers = await User.find({ username })
+  if (existingUsers.length) {
+    return ctx.throw(400, 'User already exists')
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10)
+  const user = new User({ username, passwordHash })
+  await user.save()
+  ctx.body = {
+    token: jwt.sign(user.safeProps(), jwtSecret),
   }
 }
--- site/server/api/index.js
+++ site/server/api/index.js
@@ -1,16 +1,20 @@
 const koaJwt = require('koa-jwt')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
-const { login } = require('./auth')
+const mongoose = require('mongoose')
+const { login, register } = require('./auth')
 
 const apiRouter = new Router({
   prefix: '/api',
 })
 apiRouter.use(bodyParser())
 apiRouter.post('/login', login)
+apiRouter.post('/register', register)
 apiRouter.use(koaJwt({ secret: 'secret' }))
 apiRouter.get('/me', ctx => {
   ctx.body = ctx.state.user
 })
 
 module.exports = apiRouter
+
+mongoose.connect('mongodb://localhost:27017/buildedh')
