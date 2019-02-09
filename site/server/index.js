e4af2b4cc06ee35c858bcb35de7e3ea3bcf4ddd3
ahaurw01 <aaron.haurwitz@gmail.com>
2019-02-08 21:20:12 -0500

    uncomment build stuff

--- site/server/index.js
+++ site/server/index.js
@@ -19,18 +19,18 @@ async function start() {
     port = process.env.PORT || 3000,
   } = nuxt.options.server
 
-  // // Build in development
-  // if (config.dev) {
-  //   const builder = new Builder(nuxt)
-  //   await builder.build()
-  // }
-  //
-  // app.use(ctx => {
-  //   ctx.status = 200
-  //   ctx.respond = false // Bypass Koa's built-in response handling
-  //   ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
-  //   nuxt.render(ctx.req, ctx.res)
-  // })
+  // Build in development
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  app.use(ctx => {
+    ctx.status = 200
+    ctx.respond = false // Bypass Koa's built-in response handling
+    ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
+    nuxt.render(ctx.req, ctx.res)
+  })
 
   app.listen(port, host)
   consola.ready({
