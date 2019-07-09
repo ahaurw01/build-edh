ab5951957fc8781e23da696c4d8eae505a263740
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2019-07-09 08:59:38 -0400

    render a card section

---
+++ site/components/CardSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h4 class="title is-4">
+      {{ title }}
+    </h4>
+
+    <Card
+      v-for="card in cards"
+      :key="card.source.name"
+      :card="card.source"
+      size="medium"
+      @click.prevent="$emit('edit-card', card)"
+    />
+  </div>
+</template>
+
+<script>
+import Card from '~/components/Card'
+export default {
+  components: {
+    Card,
+  },
+
+  props: {
+    title: { type: String, required: true },
+    cards: { type: Array, required: true },
+  },
+
+  methods: {},
+}
+</script>
+
+<style scoped></style>
