bdaff33f8e7213cb86a90bbe342f7265f6717476
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2020-01-11 12:25:55 -0500

    start org into proper cols

--- site/components/CardSection.vue
+++ site/components/CardSection.vue
@@ -5,17 +5,27 @@
       <BIcon v-if="isAuto" icon="flash" />
     </h4>
 
-    <div class="cards-container">
-      <div v-for="card in cards" :key="card.source.name" class="card-container">
-        <button
-          class="card-container-inner"
-          @click="
-            isCardShowcaseOpen = true
-            cardToShowcase = card
-          "
+    <div class="card-columns">
+      <div
+        v-for="column in columns"
+        :key="column[0].source.name"
+        class="cards-container"
+      >
+        <div
+          v-for="card in column"
+          :key="card.source.name"
+          class="card-container"
         >
-          <Card :card="card.source" :count="card.count" size="medium" />
-        </button>
+          <button
+            class="card-container-inner"
+            @click="
+              isCardShowcaseOpen = true
+              cardToShowcase = card
+            "
+          >
+            <Card :card="card.source" :count="card.count" size="medium" />
+          </button>
+        </div>
       </div>
     </div>
 
@@ -38,6 +48,7 @@
 </template>
 
 <script>
+import chunk from 'lodash/chunk'
 import Card from '~/components/Card'
 export default {
   components: {
@@ -54,14 +65,59 @@ export default {
     return {
       isCardShowcaseOpen: false,
       cardToShowcase: null,
+      numColumns: 1,
     }
   },
 
-  methods: {},
+  computed: {
+    columns() {
+      return chunk(this.cards, Math.ceil(this.cards.length / this.numColumns))
+    },
+  },
+
+  mounted() {
+    this.oneColumnWatcher = window.matchMedia('(max-width: 768px)')
+    this.twoColumnWatcher = window.matchMedia(
+      '(min-width: 769px) and (max-width: 999px)'
+    )
+    this.threeColumnWatcher = window.matchMedia('(min-width: 999px)')
+
+    this.oneColumnWatcher.addListener(this.watchForOneColumn)
+    this.twoColumnWatcher.addListener(this.watchForTwoColumns)
+    this.threeColumnWatcher.addListener(this.watchForThreeColumns)
+
+    this.watchForOneColumn(this.oneColumnWatcher)
+    this.watchForTwoColumns(this.twoColumnWatcher)
+    this.watchForThreeColumns(this.threeColumnWatcher)
+  },
+
+  beforeDestroy() {
+    this.oneColumnWatcher.removeListener(this.watchForOneColumn)
+    this.twoColumnWatcher.removeListener(this.watchForTwoColumns)
+    this.threeColumnWatcher.removeListener(this.watchForThreeColumns)
+  },
+
+  methods: {
+    watchForOneColumn: function(e) {
+      if (e.matches) this.numColumns = 1
+    },
+
+    watchForTwoColumns: function(e) {
+      if (e.matches) this.numColumns = 2
+    },
+
+    watchForThreeColumns: function(e) {
+      if (e.matches) this.numColumns = 3
+    },
+  },
 }
 </script>
 
 <style scoped>
+.card-columns {
+  display: flex;
+}
+
 .cards-container {
   display: flex;
   flex-direction: column;
