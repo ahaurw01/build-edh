7a0d167de8362a6f8110269cf42cbed997169ce2
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2020-01-14 11:16:51 -0500

    make customizable type component

---
+++ site/components/CompuPurposes/FlexType.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="type">
+    <div v-if="editing">
+      <div
+        v-for="(condition, index) in conditions"
+        :key="condition.type + index"
+        class="level is-mobile"
+      >
+        <div class="level-item">is:</div>
+        <div class="level-item">
+          <BSelect
+            :placeholder="`Select ${typeDisplayName}`"
+            :value="condition.type"
+            @input="onSelectType(index, $event)"
+          >
+            <option v-for="type in types" :key="type" :value="type">
+              {{ type }}
+            </option>
+          </BSelect>
+        </div>
+
+        <div class="level-item">
+          <BButton
+            :disabled="conditions.length < 2"
+            type="is-danger"
+            icon-right="delete"
+            @click="deleteCondition(index)"
+          />
+        </div>
+      </div>
+      <div class="level">
+        <div class="level-item">
+          <button
+            v-if="canAddAnotherCondition"
+            class="button"
+            @click="addAnotherCondition"
+          >
+            Or...
+          </button>
+        </div>
+      </div>
+    </div>
+    <span v-else>{{ displayString }}</span>
+  </div>
+</template>
+
+<script>
+import get from 'lodash/get'
+import last from 'lodash/last'
+
+export default {
+  props: {
+    types: {
+      type: Array,
+      required: true,
+    },
+    typeDisplayName: {
+      type: String,
+      required: true,
+    },
+    conditions: {
+      type: Array,
+      default() {
+        return [{}]
+      },
+    },
+    editing: {
+      type: Boolean,
+      default() {
+        return false
+      },
+    },
+  },
+
+  computed: {
+    canAddAnotherCondition() {
+      return this.editing && !!get(last(this.conditions), 'type')
+    },
+
+    displayString() {
+      return `is ${this.conditions.map(c => c.type).join(' or ')}`
+    },
+  },
+
+  methods: {
+    onSelectType(index, type) {
+      this.$emit('onConditionsChange', [
+        ...this.conditions.slice(0, index),
+        { type },
+        ...this.conditions.slice(index + 1),
+      ])
+    },
+
+    addAnotherCondition() {
+      this.$emit('onConditionsChange', [...this.conditions, {}])
+    },
+
+    deleteCondition(index) {
+      this.$emit('onConditionsChange', [
+        ...this.conditions.slice(0, index),
+        ...this.conditions.slice(index + 1),
+      ])
+    },
+  },
+}
+</script>
+
+<style scoped>
+.type {
+  display: inline;
+}
+</style>
--- site/components/CompuPurposes/Type.vue
+++ site/components/CompuPurposes/Type.vue
@@ -1,52 +1,14 @@
 <template>
-  <div class="type">
-    <div v-if="editing">
-      <div
-        v-for="(condition, index) in conditions"
-        :key="condition.type + index"
-        class="level is-mobile"
-      >
-        <div class="level-item">is:</div>
-        <div class="level-item">
-          <BSelect
-            placeholder="Select a type"
-            :value="condition.type"
-            @input="onSelectType(index, $event)"
-          >
-            <option v-for="type in types" :key="type" :value="type">
-              {{ type }}
-            </option>
-          </BSelect>
-        </div>
-
-        <div class="level-item">
-          <BButton
-            :disabled="conditions.length < 2"
-            type="is-danger"
-            icon-right="delete"
-            @click="deleteCondition(index)"
-          />
-        </div>
-      </div>
-      <div class="level">
-        <div class="level-item">
-          <button
-            v-if="canAddAnotherCondition"
-            class="button"
-            @click="addAnotherCondition"
-          >
-            Or...
-          </button>
-        </div>
-      </div>
-    </div>
-    <span v-else>{{ displayString }}</span>
-  </div>
+  <FlexType
+    v-bind="$props"
+    :types="types"
+    type-display-name="Type"
+    v-on="$listeners"
+  />
 </template>
 
 <script>
-import get from 'lodash/get'
-import last from 'lodash/last'
+import FlexType from './FlexType'
 
 const types = [
   'Instant',
@@ -59,6 +21,7 @@ const types = [
   'Tribal',
 ]
 export default {
+  components: { FlexType },
   props: {
     conditions: {
       type: Array,
@@ -77,42 +40,7 @@ export default {
   data() {
     return { types }
   },
-
-  computed: {
-    canAddAnotherCondition() {
-      return this.editing && !!get(last(this.conditions), 'type')
-    },
-
-    displayString() {
-      return `is ${this.conditions.map(c => c.type).join(' or ')}`
-    },
-  },
-
-  methods: {
-    onSelectType(index, type) {
-      this.$emit('onConditionsChange', [
-        ...this.conditions.slice(0, index),
-        { type },
-        ...this.conditions.slice(index + 1),
-      ])
-    },
-
-    addAnotherCondition() {
-      this.$emit('onConditionsChange', [...this.conditions, {}])
-    },
-
-    deleteCondition(index) {
-      this.$emit('onConditionsChange', [
-        ...this.conditions.slice(0, index),
-        ...this.conditions.slice(index + 1),
-      ])
-    },
-  },
 }
 </script>
 
-<style scoped>
-.type {
-  display: inline;
-}
-</style>
+<style scoped></style>
