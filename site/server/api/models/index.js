cfd9317b0467ca4cbfe3c4d49da9bb1634030000
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2020-01-15 10:02:25 -0500

    forgot loyalty in card schema

--- site/server/api/models/index.js
+++ site/server/api/models/index.js
@@ -64,6 +64,7 @@ const cardSchema = new Schema({
       power: String,
       toughness: String,
       colors: [String],
+      loyalty: String,
     },
   ],
 })
@@ -76,6 +77,7 @@ const types = [
   'Planeswalker',
   'Instant',
   'Sorcery',
+  'Tribal',
 ]
 cardSchema.statics.typeLineToTypes = function(typeLine = '') {
   return typeLine
@@ -163,6 +165,7 @@ cardSchema.statics.upsertCardFromScryfallData = function(rawCard) {
       power: rawFace.power,
       toughness: rawFace.toughness,
       colors: rawFace.colors,
+      loyalty: rawFace.loyalty,
     })),
   }
   doc.canBeCommander = Card.canBeCommander(doc)
