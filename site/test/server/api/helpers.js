530723879dba8e7065a89ff1233ab3a2bf42bcc0
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2020-01-28 09:37:57 -0500

    fix mock login

--- site/test/server/api/helpers.js
+++ site/test/server/api/helpers.js
@@ -1,4 +1,5 @@
 const supertest = require('supertest')
+const DiscordOauth2 = require('discord-oauth2')
 const { User } = require('../../../server/api/models')
 const { app } = require('../../../server')
 const server = app.callback()
@@ -7,16 +8,20 @@ module.exports = { mockLogin }
 
 async function mockLogin() {
   jest
-    .spyOn(User, 'findOne')
+    .spyOn(User, 'findOneAndUpdate')
     .mockImplementation(() =>
       Promise.resolve(new User({ id: 1, username: 'aaron' }))
     )
 
-  const {
-    body: { token },
-  } = await supertest(server)
-    .post('/api/login')
-    .send({ username: 'aaron', password: 'password' })
+  DiscordOauth2.prototype.tokenRequest = DiscordOauth2.prototype.getUser = function() {
+    return Promise.resolve({})
+  }
 
-  return `Bearer ${token}`
+  const {
+    body: { access_token },
+  } = await supertest(server)
+    .post('/api/login/discord')
+    .send({ code: 'mock code' })
+
+  return `Bearer ${access_token}`
 }
